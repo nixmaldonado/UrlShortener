7af2b085a46d9b756842cde62069fac86c01ccd0
Nicolas Maldonado <nixmaldonado@gmail.com>
2025-07-02 10:06:54 -0300

    Implements redirect handler

--- handlers.go
+++ handlers.go
@@ -8,10 +8,10 @@ import (
 const ShortCode = "short_code"
 
 type Payload struct {
-	LongUrl string `json:"long_url"`
+	LongURL string `json:"long_url"`
 }
 
-func handlerShorten(c *gin.Context) {
+func handlerShorten(c *gin.Context, storage *URLStorage) {
 	p := new(Payload)
 	err := c.ShouldBindJSON(&p)
 
@@ -20,8 +20,18 @@ func handlerShorten(c *gin.Context) {
 		return
 	}
 
-	if isValidUrl(p.LongUrl) {
+	if isValidUrl(p.LongURL) {
+		shortCode := generateShortCode(p.LongURL)
 
+		shortCode, err := storage.Store(shortCode, p.LongURL)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"short_url": shortCode,
+		})
 		return
 	}
 
@@ -29,6 +39,19 @@ func handlerShorten(c *gin.Context) {
 	return
 }
 
-func handleRedirect(c *gin.Context) {
+func handleRedirect(c *gin.Context, storage *URLStorage) {
+	shortCode := c.Param(ShortCode)
 
+	longUrl, err := storage.Get(shortCode)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	if longUrl == "" {
+		c.JSON(http.StatusNotFound, gin.H{"error": "URL not found"})
+		return
+	}
+
+	c.Redirect(http.StatusFound, longUrl)
 }
