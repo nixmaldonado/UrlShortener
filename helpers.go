588c605e82b2e4020690a33193b137a531c63856
Nicolas Maldonado <nixmaldonado@gmail.com>
2025-07-02 10:07:30 -0300

    Adds generateShortCode function in helpers

--- helpers.go
+++ helpers.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"hash/crc32"
 	"net/url"
 )
+
+const CodeSize = 7
 
 func isValidUrl(longUrl string) bool {
 	_, err := url.ParseRequestURI(longUrl)
 	return err == nil
+}
+func generateShortCode(longURL string) string {
+	hash := crc32.ChecksumIEEE([]byte(longURL))
+	hashString := fmt.Sprintf("%010d", hash)
+
+	return hashString[:CodeSize]
 }
